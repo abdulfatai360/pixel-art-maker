2be5e4a88e4398c30382897fdb125a182054a0d6
Oluphetty <oluphetty@gmail.com>
2018-06-30 09:39:35 +0100

    refactor: Modify code to optimize features

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 /*
-  TODO: Add Functionality for Toggling Menu and Toolbar Display
-  ----------------------------------------------------------------
+  TODO: Implement Feature that Toggles Menu and Toolbar Display
+  --------------------------------------------------------------
 */
 const MENU_ICON = document.querySelector('.menu-item');
-// const TOOLBAR_CONT = document.querySelector('.toolbar-container');
-// const APP_TITLE = document.querySelector('.app-title');
 
 MENU_ICON.addEventListener('click', function() {
   const HEADER = document.querySelector('header');
@@ -12,88 +10,199 @@ MENU_ICON.addEventListener('click', function() {
 });
 
 /*
-  TODO: Implement "makeGrid()" to Dynamically Create Canvas Grids
+  TODO: Implement Feature that Grabs and Stores Active Color Value
   -----------------------------------------------------------------
 */
-function makeGrid() {
-  const INPUT_HEIGHT = document.querySelector('#inputHeight');
-  const INPUT_WIDTH = document.querySelector('#inputWidth');
-  const PIXEL_CANVAS = document.querySelector('#pixelCanvas');
+let activeColor;
 
-  while (PIXEL_CANVAS.firstChild) {
-    PIXEL_CANVAS.removeChild(PIXEL_CANVAS.firstChild);
-  }
-
-  let gridHeight = INPUT_HEIGHT.value;
-  let gridWidth = INPUT_WIDTH.value;
-
-  for (let i = 1; i <= gridHeight; i++) {
-    const TR = document.createElement('tr');
-    PIXEL_CANVAS.appendChild(TR);
-    for (let j = 1; j <= gridWidth; j++) {
-      const TD = document.createElement('td');
-      TR.appendChild(TD);
-    }
-  }
-
-  // Set the number fields value back to the allowable max value 
-  INPUT_HEIGHT.value = INPUT_HEIGHT.max;
-  INPUT_WIDTH.value = INPUT_WIDTH.max;
-  activeColor();
-}
-
-//  TODO: Add Event Listener to a button to create the design canvas
-const CANVAS_CREATOR = document.querySelector('input[type=button]');
-CANVAS_CREATOR.addEventListener('click', makeGrid);
-
-/*
-  TODO: Implement Feature that Dynamically Paint Grid
-  -------------------------------------------------------------------
-*/
-// NOTE: I use the "var" keyword here for Hoisting advantage
-var color;
-function activeColor() {
+function colorToBeUsed() {
   const COLOR_PICKER = document.querySelector('#colorPicker');
-  color = COLOR_PICKER.value;
-  console.log('activeColor', color);
+  activeColor = COLOR_PICKER.value;
+  console.log('activeColor', activeColor);
 }
 
 // TODO: Block-scoped the "COLOR_PICKER" variable. I use this technique
 // in the code going forward to avoid littering the global context.
 {
   const COLOR_PICKER = document.querySelector('#colorPicker');
-  COLOR_PICKER.addEventListener('change', activeColor);
-}
-
-function paintGrid(event) {
-  let activeGrid = event.target;
-  if (activeGrid.nodeName === 'TD') {
-    activeGrid.style.backgroundColor = color;
-  }
-}
-
-// TODO: Add Event Listeners to grid using event delegation technique
-{
-  const PIXEL_CANVAS = document.querySelector('#pixelCanvas');
-  PIXEL_CANVAS.addEventListener('click', paintGrid);
+  COLOR_PICKER.addEventListener('change', colorToBeUsed);
 }
 
 /*
-  TODO: Implement Feature that Remove the Color on a Grid
+  TODO: Implement Feature that Creates and Clears Canvas Grids
   -------------------------------------------------------------
 */
-function removeGridPaint() {
-  let activeGrid = event.target;
-  if (activeGrid.nodeName === 'TD') {
-    activeGrid.style.backgroundColor = '';
+function makeGrid() {  
+  const HEIGHT_FIELD = document.querySelector('#inputHeight');
+  const WIDTH_FIELD = document.querySelector('#inputWidth');
+  const PIXEL_CANVAS = document.querySelector('#pixelCanvas');
+
+  while (PIXEL_CANVAS.firstChild) {
+    PIXEL_CANVAS.removeChild(PIXEL_CANVAS.firstChild);
+  }
+
+  let inputHeight = HEIGHT_FIELD.value;
+  let inputWidth = WIDTH_FIELD.value;
+
+  for (let i = 1; i <= inputHeight; i++) {
+    const TR = document.createElement('tr');
+    PIXEL_CANVAS.appendChild(TR);
+
+    for (let j = 1; j <= inputWidth; j++) {
+      const TD = document.createElement('td');
+      TR.appendChild(TD);
+    }
+  }
+
+  // Set the number fields value back to the allowable max value
+  HEIGHT_FIELD.value = HEIGHT_FIELD.max;
+  WIDTH_FIELD.value = WIDTH_FIELD.max;
+
+  colorToBeUsed();
+
+  PIXEL_CANVAS.addEventListener('mouseover', function(event) {
+    if (event.target.nodeName === 'TD') {
+      event.target.classList.add('on-mode');
+    }
+  });
+
+  PIXEL_CANVAS.addEventListener('mouseout', function (event) {
+    if (event.target.nodeName === 'TD') {
+      event.target.classList.remove('on-mode');
+    }
+  });
+}
+
+const WIDTH_FIELD = document.querySelector('#inputWidth');
+WIDTH_FIELD.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    makeGrid();
+  }
+});
+
+const CANVAS_CREATOR = document.querySelector('input[type=button]');
+CANVAS_CREATOR.addEventListener('click', makeGrid);
+
+/*
+  TODO: Implement Feature that Dynamically Paint Grid
+  ----------------------------------------------------
+*/
+const DESIGN_TOOLS = document.querySelectorAll('.tool');
+
+function hiLiteActiveTool() {
+  for (let i = 0; i < DESIGN_TOOLS.length; i++) {
+    if (this === DESIGN_TOOLS[i]) {
+      DESIGN_TOOLS[i].classList.add('activated');
+    } else {
+      DESIGN_TOOLS[i].classList.remove('activated');
+    }
   }
 }
 
-// TODO: Add Event Listeners to grid using event delegation technique
-{
+function paintToolActivated() {
   const PIXEL_CANVAS = document.querySelector('#pixelCanvas');
+  PIXEL_CANVAS.classList.remove('erase-mode', 'eye-dropper-mode');
+
+  let boundHiLiteActiveTool = hiLiteActiveTool.bind(PAINT_TOOL);
+  boundHiLiteActiveTool();
+
+  function paintGrid(event) {
+    let activeGrid = event.target;
+    if (activeGrid.nodeName === 'TD' &&
+      PAINT_TOOL.classList.contains('activated')) {
+      activeGrid.style.backgroundColor = activeColor;
+    }
+  }
+
+  PIXEL_CANVAS.classList.add('paint-mode');
   PIXEL_CANVAS.addEventListener('click', paintGrid);
 }
+
+const PAINT_TOOL = document.querySelector('.paint-tool');
+PAINT_TOOL.addEventListener('click', paintToolActivated);
+PAINT_TOOL.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    paintToolActivated();
+  }
+});
+
+/*
+  TODO: Implement Feature that Remove Color on a Grid
+  -------------------------------------------------------------
+*/
+function eraseToolActivated() {
+  const PIXEL_CANVAS = document.querySelector('#pixelCanvas');
+  PIXEL_CANVAS.classList.remove('paint-mode', 'eye-dropper-mode');
+
+  let boundHiLiteActiveTool = hiLiteActiveTool.bind(ERASE_TOOL);
+  boundHiLiteActiveTool();
+
+  function removeGridPaint(event) {
+    let activeGrid = event.target;
+    if (activeGrid.nodeName === 'TD' &&
+      ERASE_TOOL.classList.contains('activated')) {
+      activeGrid.style.backgroundColor = '';
+    }
+  }
+
+  PIXEL_CANVAS.classList.add('erase-mode');
+  PIXEL_CANVAS.addEventListener('click', removeGridPaint);
+}
+
+const ERASE_TOOL = document.querySelector('.erase-tool');
+ERASE_TOOL.addEventListener('click', eraseToolActivated);
+ERASE_TOOL.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    eraseToolActivated();
+  }
+});
+
+/*
+  TODO: Implement Feature that Picks Color Dynamically on the Canvas  *** Color Picker Effect ***
+  -------------------------------------------------------------------
+*/
+function eyeDropperToolActivated() {
+  const PIXEL_CANVAS = document.querySelector('#pixelCanvas');
+  PIXEL_CANVAS.classList.remove('paint-mode', 'erase-mode');
+
+  let boundHiLiteActiveTool = hiLiteActiveTool.bind(EYE_DROPPER_TOOL);
+  boundHiLiteActiveTool();
+
+  function rgbToHex(rgbArr) {
+    let hexArr = rgbArr.map(rgbColorChannel => {
+      let hexColorChannel = Number(rgbColorChannel).toString(16);
+      return (hexColorChannel.length === 1) ? '0' + hexColorChannel : hexColorChannel
+    });
+    return '#' + hexArr.join('');
+  }
+
+  function eyeDropper(event) {
+    const COLOR_PICKER = document.querySelector('#colorPicker');
+    let activeGrid = event.target;
+    let colorToBePicked = activeGrid.style.backgroundColor;
+
+    if (activeGrid.nodeName === 'TD' &&
+      colorToBePicked !== '' &&
+      EYE_DROPPER_TOOL.classList.contains('activated')) {
+      colorInRgb = colorToBePicked
+        .replace(/[rgb()\s]/gi, '')
+        .split(',');
+      COLOR_PICKER.value = rgbToHex(colorInRgb);
+      activeColor = COLOR_PICKER.value;
+    }
+  }
+
+  PIXEL_CANVAS.classList.add('eye-dropper-mode');
+  PIXEL_CANVAS.addEventListener('click', eyeDropper);
+}
+
+const EYE_DROPPER_TOOL = document.querySelector('.eye-dropper-tool');
+EYE_DROPPER_TOOL.addEventListener('click', eyeDropperToolActivated);
+EYE_DROPPER_TOOL.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    eyeDropperToolActivated();
+  }
+});
 
 /*
   TODO: Implement Feature that Removes all Colored Grids on the Canvas
@@ -101,7 +210,7 @@ function removeGridPaint() {
 */
 function clearCanvas() {
   let wantToClear = window.confirm(
-    '\nWARNING MESSAGE:\n' + 'Do you sure want to clear the Design Canvas?'
+    '\nWARNING MESSAGE:\n\n' + 'Do you want to clear the design canvas?'
   );
   
   if (wantToClear) {
@@ -113,3 +222,11 @@ function clearCanvas() {
     return;
   }
 }
+
+const CLEAR_CANVAS_TOOL = document.querySelector('.clear-canvas-tool');
+CLEAR_CANVAS_TOOL.addEventListener('click', clearCanvas);
+CLEAR_CANVAS_TOOL.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    clearCanvas();
+  }
+});
